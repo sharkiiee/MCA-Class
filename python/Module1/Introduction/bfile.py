68268affba6a1704e0733912bc2c077a7fefc7b4
sharkiiee <sarthak231203@gmail.com>
2024-09-12 09:58:27 +0530

    adding python files

---
+++ python/Module1/Introduction/bfile.py
@@ -0,0 +1,7 @@
+# Python program to add two numbers
+
+a = input("Enter the first value ")
+
+b = input("enter the second value ")
+
+print("sum = ",int(a)+int(b))
---
+++ python/Module1/Introduction/cfile.py
@@ -0,0 +1,8 @@
+# python program to find square root
+
+import math
+
+num = int(input("enter the value: "))
+srt = math.sqrt(num)
+
+print("the square root of the number is : ", srt)
