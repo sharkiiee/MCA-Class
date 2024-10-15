43d6ed28d67ad7da8a793eb180bb50b73d36b164
sharkiiee <sarthak231203@gmail.com>
2024-10-15 10:46:01 +0530

    adding more files

---
+++ python/pr.py
@@ -0,0 +1,19 @@
+# enter the fruits value in tuple
+
+# store the record of the 3 students
+
+student = {
+ 
+}
+
+for _ in range(2):
+    name = input("name : ")
+    values = input("value: ")
+    passwords = input("password: ")
+
+    student[name]={
+        "value":values,
+        "password":passwords
+    }
+
+print(student)
---
+++ web-dev/Lab-5/04file.js
@@ -0,0 +1,7 @@
+// check  whether the program is palindrome or not
+
+let userValue = 131;
+save = userValue;
+while(userValue != 0){
+    
+}
